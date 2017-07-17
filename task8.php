fbf559110b31abfe80a8bcae805f64a2bf2a73ff
Philip Maksiutov <philip_bmx11@yahoo.com>
2017-07-17 20:57:00 +0300

    Подправлены 7-я и 8-я задачи ( но не до конца)

--- task8.php
+++ task8.php
@@ -1,16 +1,23 @@
 <?php
 
 $str = "String variable";
+$str2 = "Hello world 12345 67890";
+
 
 echo $str . "<br>";
 echo $str . "<br>";
 
 $array = str_split($str);
 $array2 = str_split($str, 2);
+$array3 = str_split($str2, 6);
 
 
 echo '<pre>';
 print_r($array);
+echo '</pre>';
+
+echo '<pre>';
+print_r($array3);
 echo '</pre>';
 
 echo '<pre>';
